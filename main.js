cb8c90525dcf593997536bceef8d1bbf857e90c6
neubaguette <romanturuntaiev2023@gmail.com>
2024-04-23 08:37:14 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+let myImg = document.querySelector('img');
+
+myImg.onclick = function(){
+    let mySrc = myImg.getAttribute('src');
+    if(mySrc==='images/firefox-icon.png'){
+        myImg.setAttribute('src','images/google.png');
+    }else{
+        myImg.setAttribute('src','images/firefox-icon.png')
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+    let myName = prompt('Please enter your name!');
+    if(!myName){
+        setUserName();
+    }else{
+        localStorage.setItem('name',myName);
+        myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+} else{
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function(){
+    setUserName();
+}
